40e45cbd675e411748db860a417e0bb9ae24acce
GoldDeniel <aranyadhun@gmail.com>
2022-03-09 13:28:14 +0100

    Working but alsa needs fix because it crashes

--- CiscoDiscoClient.py
+++ CiscoDiscoClient.py
@@ -1,8 +1,9 @@
 import socket
+from struct import pack
 import sys
 import time
 import string
-
+import time
 
 #getting setting target ip
 if len(sys.argv) == 3:
@@ -13,36 +14,38 @@ else:
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
 
-global timeToSleep
-
 import sounddevice as sd
 import numpy as np
 
-def print_sound(indata, outdata, frames, time, status):
+def send_packages(deci):
+
+    #letters = string.ascii_letters
+    #send_data = ''.join(random.choice(letters) for i in range(random.randint(1,100)))
+    send_data = 'package'
+    time.sleep(.0001)
+    s.sendto(send_data.encode('utf-8'), (ip, port))
+    print("Sent data: ", send_data, " ", deci)
+
+def print_sound(indata, outdata, frames, time_, status):
     volume_norm = np.linalg.norm(indata)*10
     
-    print ("|" * int(volume_norm - 6))
-    #timeToSleep = (int(volume_norm) * .0001)
-    #print(timeToSleep)
-
-with sd.Stream(callback=print_sound):
-    sd.sleep(100000)
+    #print ("|" * int(volume_norm - 6))
+    #outputVolume = (int(volume_norm) * .0001)
     
 
 
 
+    outputVolume = int(volume_norm)
+    #print(outputVolume)
 
+    for x in range(round(outputVolume * 0.5)):
+        send_packages(outputVolume)
+with sd.Stream(callback=print_sound):
+    sd.sleep(100000)
 
-# sending
+#sending
 
-# while True:
-#     get_speaker_output_volume()
-# while True:
+#while True:
+    #get_speaker_output_volume()
 
-#     letters = string.ascii_letters
-#     #send_data = ''.join(random.choice(letters) for i in range(random.randint(1,100)))
-#     send_data = 'package'
-#     time.sleep(.0001)
-#     s.sendto(send_data.encode('utf-8'), (ip, port))
-#     print("Sent data: ", send_data)
 s.close()
--- outputtest.py
+++ outputtest.py
@@ -1,11 +1,23 @@
-import alsaaudio
+import socket
+import sys
+import time
 
-def get_volume():
-    mixer = alsaaudio.Mixer()
-    pcm = alsaaudio.PCM()
-    print(pcm)
-    vol = mixer.getvolume()
-    #return vol
+import sounddevice as sd
+import numpy as np
+
+#getting setting target ip
+if len(sys.argv) == 3:
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
+else:
+    print("Run like : python3 client.py <arg1 server ip 192.168.1.102> <arg2 server port 4444 >")
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
 
 while True:
-    print(get_volume())
+    letters = ''
+    #send_data = ''.join(random.choice(letters) for i in range(random.randint(1,100)))
+    send_data = 'packaasfjasfiojasfpjasfpajsfpoiage'
+    time.sleep(.00001)
+    s.sendto(send_data.encode('utf-8'), (ip, port))
+    print("Sent data: ", send_data)
